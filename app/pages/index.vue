2d7d8fff314465a94fcb02c6840b88c99aee9db7
homahi <homahi0128@gmail.com>
2018-12-29 10:40:11 +0900

    nuxt/typescript環境を作ってみた

---
+++ app/pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <section>
+    <h1 class="header">Nuxt TypeScript Starter</h1>
+    <div class="cards">HelloWorld</div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator";
+import { State } from "vuex-class";
+import Card from "~/components/Card.vue";
+
+@Component({
+  components: {
+    Card
+  }
+})
+export default class extends Vue {
+  @State people;
+
+  asyncData({ store }) {
+    console.log(store);
+    store.dispatch("initialize");
+  }
+}
+</script>
+<style scoped>
+.header {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.cards {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
